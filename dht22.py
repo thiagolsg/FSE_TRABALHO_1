c74abaf25655d5984c686fe1aa72a2874522e003
thiagomesUNB <thiago.luiz20000@gmail.com>
2022-12-16 16:05:42 -0300

    Alterações no codigo do dht22

--- dht22.py
+++ dht22.py
@@ -5,15 +5,11 @@ import board
 dht_device = adafruit_dht.DHT22(board.D4,use_pulseio=False)
 
 def dht22():
-    while True:
-        try:
-            temperature = dht_device.temperature
-            humidity = dht_device.humidity
-        except RuntimeError as error:
-            print(error.args[0])
-            #print('não foi possivel ler')
-            time.sleep(2)
-            continue
+    try:
+        temperature = dht_device.temperature
+        humidity = dht_device.humidity
+    except RuntimeError as error:
+        return error.args[0]
 
-        time.sleep(2)
-        print(f"temperatura = {temperature}, humidade = {humidity}")
+    return f"temperatura = {temperature}, humidade = {humidity}"
+    
