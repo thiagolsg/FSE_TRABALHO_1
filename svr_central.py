2cd17a942c46263090ccc146db0c9e5bc728303d
thiagomesUNB <thiago.luiz20000@gmail.com>
2022-12-17 23:24:42 -0300

    Configurando csv e threads para conexão de mais de uma placa

--- svr_central.py
+++ svr_central.py
@@ -2,6 +2,9 @@ from socket import *
 import os
 from configuracao import ip_central,porta_central,ip_distribuido,porta_distribuido,nome_sala
 import time
+import csv
+from datetime import datetime
+import threading
 
 svr_central = socket(AF_INET, SOCK_STREAM)
 svr_central.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1) #teste
@@ -9,44 +12,60 @@ svr_central.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1) #teste
 svr_central.bind((ip_central, porta_central))
 svr_central.listen(5)
 
-print(f'Servidor Central conectado no HOST:  {ip_central} e PORTA: {porta_central}')
+print(f'Servidor Central:  {ip_central} e Porta: {porta_central}')
 
-svr_distribuido, svr_distribuido_ip = svr_central.accept()
+lista_svr_distribuido = []
+svr_distribuido_conexao = ''
 
-print(f'O Servidor distribuido: {svr_distribuido_ip} se conectou')
+def recebe_conexao():
+    try:
+        while True:
+            svr_distribuido, svr_distribuido_ip = svr_central.accept()
+            nome_sala = svr_distribuido.recv(1024).decode()
+            lista_svr_distribuido.append({'conexao':svr_distribuido,'sala': nome_sala})
+            print(f'{svr_distribuido_ip} se conectou')
+    except RuntimeError as error:
+        return error.args[0]
 
+threading.Thread(target=recebe_conexao, ).start()
 
 while True:
+    print('====================================================')
+    print(f'============= BEM VINDO AO PRÉDIO =================')
+    for sala in lista_svr_distribuido: print(sala['sala']+' esta conectada')
     print('=================================================')
-    print(f'============= BEM VINDO A {nome_sala} =================')
-    print('=================================================')
-
     
     entrada_usuario = str(input('VER DISPOSITIVOS DE ENTRADA (1)\nVER DISPOSITIVOS DE SAÍDA (2)\nVER VALORES DE TEMPERATURA E UMIDADE (3)\nACIONAR DISPOSITIVOS (4)\n'))
-    svr_distribuido.send(entrada_usuario.encode())
 
     if entrada_usuario != '4':
         try:
             os.system('clear')
+
+            for sala in lista_svr_distribuido: print(sala['sala'])
+            numero_sala = str(input('Digite o número da sala que deseja\n'))
+
+            with open('log.csv','a') as csvfile:
+                evento = f'Visualizando Dispositivos de Entrada Sala {numero_sala}' if entrada_usuario == '1' else f'Visualizando Dispositivos de Saída Sala {numero_sala}' if entrada_usuario == '2' else f'Visualizando Temperatura e Humidade Sala {numero_sala}'
+                writer = csv.writer(csvfile,delimiter = ',')
+                print(datetime.now().strftime('%d/%m/%Y %H:%M')+',',evento,file = csvfile)
+            
+            for sala in lista_svr_distribuido:
+                if sala['sala'][-1] == numero_sala:
+                    svr_distribuido_conexao = sala['conexao']
+
+            svr_distribuido_conexao.send(entrada_usuario.encode())
+
             while True:
+
                 continua = 'continua'
-                svr_distribuido.send(continua.encode())
-                message_received = svr_distribuido.recv(1024)
+                svr_distribuido_conexao.sendall(continua.encode())
+                message_received = svr_distribuido_conexao.recv(1024)
                 print(message_received.decode())
                 time.sleep(2)
         except KeyboardInterrupt:
             pare = 'pare'
-            svr_distribuido.send(pare.encode())
+            svr_distribuido_conexao.send(pare.encode())
             os.system('clear')
             continue
-    
-    # else:
-    #     print("")
-
-
-    # clear_page = int(input('LIMPAR TELA (1)\n'))
-    # if clear_page == 1:
-    #     os.system('clear')
-
 
 svr_distribuido.close()
